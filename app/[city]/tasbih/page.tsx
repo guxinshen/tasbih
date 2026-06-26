295dff506a4f6eab0a38e3ef717adfb9f3d1db0a
guxinshen <liuyongzou@gmail.com>
2026-06-26 11:02:54 +0800

    Create page.tsx

---
+++ app/[city]/tasbih/page.tsx
@@ -0,0 +1,17 @@
+import TasbihClient from "./TasbihClient";
+
+export async function generateMetadata({ params }: any) {
+  return {
+    title: `Tasbih Online di ${params.city}`,
+    description: `Tasbih digital gratis untuk Muslim di ${params.city}`
+  };
+}
+
+export default function Page({ params }: any) {
+  return (
+    <main style={{ maxWidth: 700, margin: "0 auto", padding: 20 }}>
+      <h1>Tasbih Online di {params.city}</h1>
+      <TasbihClient city={params.city} />
+    </main>
+  );
+}
